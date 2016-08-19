64e8e5329e78d6f0824602a426ca9aa82be1a5fb
Abdul.Mohsen B. A. A <admin@arabcoders.org>
2016-08-19 18:05:51 +0300

    added tests bootstrap

---
+++ tests/ProviderAbstractTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class ProviderAbstractTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function testTest()
+    {
+        $this->assertSame( 'foo', 'foo' );
+    }
+}
